35536e8cd910acbcdd785e4c8aad71c721ab446d
Alex Yaro <44684541+alxyaro@users.noreply.github.com>
2023-08-31 19:25:54 -0400

    Enable the interactive pop gesture for TopBar VCs

--- Topsify/Views/Navigation/NewAppNavigationController.swift
+++ Topsify/Views/Navigation/NewAppNavigationController.swift
@@ -10,6 +10,7 @@ final class NewAppNavigationController: UINavigationController {
         super.init(rootViewController: rootViewController)
 
         delegate = self
+        interactivePopGestureRecognizer?.delegate = self
     }
 
     required init?(coder: NSCoder) {
@@ -20,6 +21,23 @@ final class NewAppNavigationController: UINavigationController {
         super.viewDidLoad()
 
     }
+
+    private func setUpTopBarIfNeeded(for viewController: UIViewController, topBarConfiguring: TopBarConfiguring) {
+        guard !viewController.view.subviews.contains(where: { $0 is TopBar }) else {
+            return
+        }
+
+        let topBar = TopBar(configurator: topBarConfiguring)
+
+        viewController.view.addSubview(topBar)
+        viewController.additionalSafeAreaInsets.top = TopBar.safeAreaHeight
+        topBar.constrainEdgesToSuperview(excluding: .bottom)
+        topBar.bottomAnchor.constraint(equalTo: viewController.view.safeAreaLayoutGuide.topAnchor).isActive = true
+
+        if let playButton = topBarConfiguring.topBarPlayButton {
+            viewController.view.addSubview(playButton)
+        }
+    }
 }
 
 extension NewAppNavigationController: UINavigationControllerDelegate {
@@ -27,22 +45,25 @@ extension NewAppNavigationController: UINavigationControllerDelegate {
     func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
         if let topBarConfiguring = viewController as? TopBarConfiguring {
             setNavigationBarHidden(true, animated: animated)
-
-            let topBar = TopBar(configurator: topBarConfiguring)
-
-            viewController.view.addSubview(topBar)
-            viewController.additionalSafeAreaInsets.top = TopBar.safeAreaHeight
-            topBar.constrainEdgesToSuperview(excluding: .bottom)
-            topBar.bottomAnchor.constraint(equalTo: viewController.view.safeAreaLayoutGuide.topAnchor).isActive = true
-
-            if let playButton = topBarConfiguring.topBarPlayButton {
-                viewController.view.addSubview(playButton)
-            }
+            setUpTopBarIfNeeded(for: viewController, topBarConfiguring: topBarConfiguring)
         } else {
             // Note: This delegate method is called after viewDidLoad/viewWillAppear, so this would override VC
             // visibility preference of the native navbar. In the future, we may want to have a regular VC
             // that hides the native navbar.
             setNavigationBarHidden(false, animated: animated)
         }
+    }
+}
+
+/// Used for the delegate of `interactivePopGestureRecognizer` to enable the
+/// pop gesture even when the native navigation bar is hidden.
+extension NewAppNavigationController: UIGestureRecognizerDelegate {
+
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        viewControllers.count > 1
+    }
+
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        true
     }
 }
