a089a6b22a9ca314ea16748f2f1a6e793b97c70c
Alex Yaro <44684541+alxyaro@users.noreply.github.com>
2024-02-12 13:28:49 -0500

    Add otherAppsPlayingAudioNotificationPublisher
    May not end up using this (as listening to AVPlayer.rate works well for
    detecting system pausing), but could be handy in the future nonetheless.

--- Topsify/Services/PlaybackManager/AudioSessionHelper.swift
+++ Topsify/Services/PlaybackManager/AudioSessionHelper.swift
@@ -1,16 +1,35 @@
 // Created by Alex Yaro on 2024-02-03.
 
 import AVFoundation
+import Combine
 import Foundation
 
 protocol AudioSessionHelperType {
+    var otherAppsPlayingAudioNotificationPublisher: AnyPublisher<Bool, Never> { get }
+
     func configureAudio() throws
     func activateAudio() async throws
 }
 
 class AudioSessionHelper: AudioSessionHelperType {
     private let audioSession = AVAudioSession.sharedInstance()
+    private let notificationCenter = NotificationCenter.default
     private let queue = DispatchQueue(label: String(describing: AudioSessionHelper.self), qos: .userInitiated)
+
+    var otherAppsPlayingAudioNotificationPublisher: AnyPublisher<Bool, Never> {
+        notificationCenter.publisher(for: AVAudioSession.silenceSecondaryAudioHintNotification)
+            .compactMap {
+                if
+                    let userInfo = $0.userInfo,
+                    let typeValue = userInfo[AVAudioSessionSilenceSecondaryAudioHintTypeKey] as? UInt,
+                    let type = AVAudioSession.SilenceSecondaryAudioHintType(rawValue: typeValue)
+                {
+                    return type == .begin ? true : false
+                }
+                return nil
+            }
+            .eraseToAnyPublisher()
+    }
 
     func configureAudio() throws {
         try audioSession.setCategory(.playback, mode: .moviePlayback)
